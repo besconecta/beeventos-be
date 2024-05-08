3edea2daa476553609751912f5edc6986552b606
Anderson Vieira <asvieira.br@gmail.com>
2024-05-08 00:10:16 -0300

    feat: cria validacoes globais

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(3000);
 }
 bootstrap();
