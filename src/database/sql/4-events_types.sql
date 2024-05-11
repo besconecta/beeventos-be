bb35ae0a7e6bb7cc9515dede4bc9df426784b7ee
Anderson Vieira <asvieira.br@gmail.com>
2024-05-11 12:02:43 -0300

    refactor: adiciona titulo do evento e descricao do tipo de evento como campos unicos

--- src/database/sql/4-events_types.sql
+++ src/database/sql/4-events_types.sql
@@ -1,6 +1,6 @@
 CREATE TABLE events_types (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    ds_type VARCHAR(100) NOT NULL,
+    ds_type VARCHAR(100) NOT NULL UNIQUE,
     dh_created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo'),
     dh_updated_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo')
 );
--- src/database/sql/5-events.sql
+++ src/database/sql/5-events.sql
@@ -2,7 +2,7 @@ CREATE TABLE events (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     event_type_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    ds_title VARCHAR(100) NOT NULL,
+    ds_title VARCHAR(100) NOT NULL UNIQUE,
     ds_about TEXT NOT NULL,
     ds_banner_url VARCHAR(255),
     ds_local VARCHAR(255) NOT NULL,
