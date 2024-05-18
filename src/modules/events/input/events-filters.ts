28148bf1baa3160118975d98c19061ec4f7ed46c
Anderson Vieira <asvieira.br@gmail.com>
2024-05-18 00:39:07 -0300

    feat(BEB-45): documenta parametros de entrada para filtro de eventos

--- src/modules/events/input/events-filters.ts
+++ src/modules/events/input/events-filters.ts
@@ -1,16 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsDateString, IsOptional } from 'class-validator';
 
 export class EventsFilters {
+  @ApiProperty({
+    name: 'title',
+    description: 'Título do evento',
+    example: 'Apresentação XYZ',
+  })
   @IsOptional()
   title: string;
 
+  @ApiProperty({
+    name: 'local',
+    description: 'Local do evento',
+    example: 'Sala ABC',
+  })
   @IsOptional()
   local: string;
 
+  @ApiProperty({
+    name: 'eventType',
+    description: 'Descrição do tipo de evento',
+    example: 'Apresentação acadêmica',
+  })
   @IsOptional()
   eventType: string;
 
+  @ApiProperty({
+    name: 'startAt',
+    description: 'Data/hora de início do evento',
+    example: '2024-05-12 20:00:00',
+  })
   @IsOptional()
   @IsDateString()
   @Transform(({ value }) => new Date(value).toISOString(), {
@@ -18,6 +39,11 @@ export class EventsFilters {
   })
   startAt: string;
 
+  @ApiProperty({
+    name: 'endAt',
+    description: 'Data/hora do fim do evento',
+    example: '2024-05-12 20:30:00',
+  })
   @IsOptional()
   @IsDateString()
   @Transform(({ value }) => new Date(value).toISOString(), {
