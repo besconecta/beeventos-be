075f47d012a3ad041192d8ea31b7848880e2767b
Anderson Vieira <asvieira.br@gmail.com>
2024-05-23 19:38:54 -0300

    refactor(BEB-46): melhora documentacao de recurso de criacao de eventos

--- src/modules/atendees/output/auth-atendee.output.ts
+++ src/modules/atendees/output/auth-atendee.output.ts
@@ -4,7 +4,7 @@ export class AuthAtendeeOutput {
   @ApiProperty({
     name: 'access_token',
     example:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1MzI1NDI2NC0zMmEwLTRhYWYtODIxZC02MmU3ZTZkNDRhZTUiLCJyb2xlIjoidXNlciIsImlhdCI6MTcxNTIxOTk5Nn0.XpEXFOBfS3p7bBWO7iC5qrcwQQ5jvTp1yE6hUZcDkiw',
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MTY1MDAwNzUsImV4cCI6MTc0ODAzNjA3NSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.HuAFMDqT74wtq_l9MZuB9gLubGI6gaVVyvVHf7tco3A',
   })
   access_token: string;
 }
--- src/modules/events/decorators/api-create-event-responses.decorator.ts
+++ src/modules/events/decorators/api-create-event-responses.decorator.ts
@@ -8,6 +8,7 @@ import {
   ApiOperation,
   ApiTags,
   ApiUnauthorizedResponse,
+  getSchemaPath,
 } from '@nestjs/swagger';
 
 import { EventOutput } from '../output';
@@ -18,39 +19,106 @@ export function ApiCreateEventResponses() {
     ApiOperation({ description: 'Cadastro evento' }),
     ApiCreatedResponse({
       description: 'Evento criado com sucesso',
-      type: EventOutput,
+      schema: {
+        type: 'object',
+        properties: {
+          data: {
+            oneOf: [{ $ref: getSchemaPath(EventOutput) }],
+          },
+        },
+      },
     }),
+
     ApiBadRequestResponse({
       description: 'Erros de validação',
       schema: {
-        example: [
-          'ID do tipo de evento é obrigatório',
-          'ID do evento com formato inválido',
-          'ID do usuário é obrigatório',
-          'ID do usuário com formato inválido',
-          'Título do evento é obrigatório',
-          'Título do evento deve conter no máximo 100 caracteres',
-          'Descrição do evento é obrigatória',
-          'Local do evento é obrigatório',
-          'Status do evento deve ser: idle, started ou finished',
-        ],
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 400 },
+          timestamp: { type: 'date', example: '14:00:00 PM' },
+          message: {
+            type: 'string',
+            example: [
+              'ID do tipo de evento é obrigatório',
+              'ID do evento com formato inválido',
+              'ID do usuário é obrigatório',
+              'Título do evento é obrigatório',
+              'Título do evento deve conter no máximo 100 caracteres',
+              'Descrição do evento é obrigatória',
+              'Local do evento é obrigatório',
+              'Status do evento deve ser: idle, started ou finished',
+            ],
+          },
+          route: {
+            type: 'string',
+            example:
+              'http://domain.com/api/v1/events/a386818e-90af-4b07-bd5d-52e21d2fb158',
+          },
+        },
       },
     }),
+
     ApiConflictResponse({
-      description:
-        'Registro duplicado: Key (field)=(description_field) already exists.',
+      description: 'Registro duplicado',
+      schema: {
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 409 },
+          timestamp: { type: 'date', example: '14:00:00 PM' },
+          message: {
+            type: 'string',
+            example:
+              'Registro duplicado: Key (field)=(description_field) already exists',
+          },
+        },
+      },
     }),
+
     ApiNotFoundResponse({
       description: 'Registros não encontrados',
       schema: {
-        example: ['Usuário não encontrado', 'Tipo de evento não encontrado'],
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 404 },
+          timestamp: { type: 'date', example: '14:00:00 PM' },
+          message: {
+            type: 'string',
+            example: [
+              'Usuário não encontrado',
+              'Tipo de evento não encontrado',
+            ],
+          },
+        },
       },
     }),
-    ApiUnauthorizedResponse({ description: 'Usuário sem permissão' }),
+
+    ApiUnauthorizedResponse({
+      description: 'Acesso negado',
+      schema: {
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 401 },
+          timestamp: { type: 'date', example: '14:00:00 PM' },
+          message: {
+            type: 'string',
+            example: 'Usuário sem permissão',
+          },
+        },
+      },
+    }),
+
     ApiInternalServerErrorResponse({
       description: 'Erro interno do servidor',
-      content: {
-        type: { example: 'Houve um erro interno ao processar solicitação' },
+      schema: {
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number', example: 500 },
+          timestamp: { type: 'date', example: '14:00:00 PM' },
+          message: {
+            type: 'string',
+            example: 'Houve um erro interno ao processar solicitação',
+          },
+        },
       },
     }),
   );
